1b1d2b76bcf09d78ac9744dff219874d25a54c7f
zinko <projectrez69420@gmail.com>
2026-02-06 21:54:49 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,82 +1,104 @@
+import { createServer } from "node:http";
+import { fileURLToPath } from "url";
+import { hostname } from "node:os";
+import { server as wisp, logging } from "@mercuryworkshop/wisp-js/server";
 import Fastify from "fastify";
 import fastifyStatic from "@fastify/static";
-import { createServer as createHttpServer } from "http";
-import { fileURLToPath } from "url";
-import { server as wisp, logging } from "@mercuryworkshop/wisp-js/server";
-import Greenlock from "greenlock-express";
-import { hostname } from "os";
 
 import { scramjetPath } from "@mercuryworkshop/scramjet/path";
 import { libcurlPath } from "@mercuryworkshop/libcurl-transport";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
 
+import Greenlock from "greenlock-express";
+
 const publicPath = fileURLToPath(new URL("../public/", import.meta.url));
 
 logging.set_level(logging.NONE);
 Object.assign(wisp.options, {
-    allow_udp_streams: false,
-    hostname_blacklist: [/example\.com/],
-    dns_servers: ["1.1.1.3", "1.0.0.3"],
+	allow_udp_streams: false,
+	hostname_blacklist: [/example\.com/],
+	dns_servers: ["1.1.1.3", "1.0.0.3"],
 });
 
 const fastify = Fastify({
-    serverFactory: (handler) => {
-        return createHttpServer()
-            .on("request", (req, res) => {
-                res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-                res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-                handler(req, res);
-            })
-            .on("upgrade", (req, socket, head) => {
-                if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
-                else socket.end();
-            });
-    },
+	serverFactory: (handler) => {
+		return createServer()
+			.on("request", (req, res) => {
+				res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+				res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+				handler(req, res);
+			})
+			.on("upgrade", (req, socket, head) => {
+				if (req.url.endsWith("/wisp/")) wisp.routeRequest(req, socket, head);
+				else socket.end();
+			});
+	},
 });
 
 fastify.register(fastifyStatic, {
-    root: publicPath,
-    decorateReply: true,
+	root: publicPath,
+	decorateReply: true,
 });
+
 fastify.register(fastifyStatic, {
-    root: scramjetPath,
-    prefix: "/scram/",
-    decorateReply: false,
+	root: scramjetPath,
+	prefix: "/scram/",
+	decorateReply: false,
 });
+
 fastify.register(fastifyStatic, {
-    root: libcurlPath,
-    prefix: "/libcurl/",
-    decorateReply: false,
+	root: libcurlPath,
+	prefix: "/libcurl/",
+	decorateReply: false,
 });
+
 fastify.register(fastifyStatic, {
-    root: baremuxPath,
-    prefix: "/baremux/",
-    decorateReply: false,
+	root: baremuxPath,
+	prefix: "/baremux/",
+	decorateReply: false,
 });
+
 fastify.setNotFoundHandler((res, reply) => {
-    return reply.code(404).type("text/html").sendFile("404.html");
+	return reply.code(404).type("text/html").sendFile("404.html");
 });
 
-function shutdown() {
-    console.log("SIGTERM signal received: closing HTTP server");
-    fastify.close();
-    process.exit(0);
-}
+fastify.server.on("listening", () => {
+	const address = fastify.server.address();
+	console.log("Listening on:");
+	console.log(`\thttp://localhost:${address.port}`);
+	console.log(`\thttp://${hostname()}:${address.port}`);
+	console.log(`\thttp://${address.family === "IPv6" ? `[${address.address}]` : address.address}:${address.port}`);
+});
 
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
-const PORT = parseInt(process.env.PORT || "80");
+function shutdown() {
+	console.log("SIGTERM signal received: closing HTTP server");
+	fastify.close();
+	process.exit(0);
+}
 
-Greenlock.init({
-    packageRoot: process.cwd(),
-    configDir: "./greenlock.d",
-    maintainerEmail: "your-email@example.com",
-    cluster: false
-})
-.ready((glx) => {
-    const httpsServer = glx.httpsServer(fastify.server);
-    httpsServer.listen(PORT, "0.0.0.0", () => {
-        console.log(`Fastify + Greenlock listening on port ${PORT} with HTTPS`);
-    });
+let port = parseInt(process.env.PORT || "80");
+if (isNaN(port)) port = 80;
+
+fastify.listen({
+	port: port,
+	host: "0.0.0.0",
+}, () => {
+	console.log(`Fastify listening on internal port ${port}`);
+});
+
+const greenlock = Greenlock.init({
+	packageRoot: process.cwd(),
+	configDir: "./greenlock.d",
+	maintainerEmail: "floppavr@gmail.com",
+	cluster: false
+});
+
+greenlock.ready((glx) => {
+	const httpsServer = glx.httpsServer(fastify.server);
+	httpsServer.listen(port, "0.0.0.0", () => {
+		console.log(`Fastify + Greenlock listening on port ${port} with HTTPS`);
+	});
 });
